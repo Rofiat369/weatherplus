99109b8681d8119b39e4e8a8364442f99e26e55c
Rofiat369 <63071451+Rofiat369@users.noreply.github.com>
2021-03-20 06:55:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -114,6 +114,9 @@ function locating(position) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showWeather);
 
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+
 }
 
 function currentLocation(event) {
